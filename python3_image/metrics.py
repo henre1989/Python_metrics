4a8166bf59f95ed4bc9d1a472528d76abe2e0902
henre1989 <transfer1989@gmail.com>
2021-03-08 20:39:26 +0500

    update3

--- python3_image/metrics.py
+++ python3_image/metrics.py
@@ -1,12 +1,19 @@
 import prometheus_client as prom
 import time
+import os
 import paramiko
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s : %(levelname)s : %(message)s', filename='metrics.log')
 
 
-def connect_to_host(host, username):
+def connect_to_host(host, username, push_to):
     user = username
     port = 22
     try:
+        logging.info('Connect start')
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname=host, username=user, port=port)
@@ -14,32 +21,45 @@ def connect_to_host(host, username):
         data = stdout.read()
         lines = (str(data).split('\\n'))
         client.close()
+        logging.info('Connect end')
         for line in lines:
             if len(line.split()) == 6:
                 fsys = line.split()[0]
                 full_size = int(line.split()[1]) * 1024
                 used_space = int(line.split()[2]) * 1024
                 mount = line.split()[-1]
-                push_metrics(host, str(fsys), full_size, used_space, str(mount))
+                push_metrics(push_to, host, str(fsys), full_size, used_space, str(mount))
+            time.sleep(1)
     except Exception as e:
-        print(e)
+        logging.info(str(e))
 
 
-def push_metrics(host, fsys, full_size, used_space, mount):
+def push_metrics(push_to, host, fsys, full_size, used_space, mount):
    registry = prom.CollectorRegistry()
    gaugeT = prom.Gauge('Total_space', 'Total space on disk in byte', registry=registry)
    gaugeU = prom.Gauge('Used_space', 'Used space from disk in byte', registry=registry)
    gaugeT.set(full_size)
    gaugeU.set(used_space)
-   prom.push_to_gateway('pushgateway:9091', job=host+' Filesystem: ' + fsys + ' ,Mount '+ mount, registry=registry)
+   logging.info(type(push_to))
+   logging.info('push_to ' + str(push_to))
+   prom.push_to_gateway(push_to, job=host+' Filesystem: ' + fsys + ' ,Mount '+ mount, registry=registry)
+   logging.info('ok')
 
 
 if __name__ == '__main__':
-    host = '10.166.0.20'
-    username = 'root'
     while True:
-        connect_to_host(host, username)
-        time.sleep(5)
+        try:
+            PATH = os.getcwd()
+            f = open(PATH + '/settings/hosts.conf', encoding='utf-8')
+            lines = f.readlines()
+            for line in lines:
+                push_to = line.split('|')[0]
+                host = line.split('|')[1]
+                username = line.split('|')[-1]
+                connect_to_host(host, username, push_to)
+                time.sleep(10)
+        except FileNotFoundError:
+            logging.info('No such file or directory')
 
 
 
